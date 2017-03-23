f096b442c20291c577a3da627ae1c5f79989496f
AbuHuraira Lakdawala <abuhuraira.l@pepperfry.com>
2017-03-23 09:42:11 +0530

    solr

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Config;
+use Solarium\Core\Client\Client;
+use Solarium_Exception;
 
 class DashboardController extends Controller
 {
@@ -19,9 +22,37 @@ class DashboardController extends Controller
         //     var_dump($order->product()->select('id')->first()->id );
         // }
         // return( $order->product );
-        \App\Facades\HtmlTags::setTitle('OFT Dashboard');
-    	$data = \App\Order::orderDashboard();
-    	return view('dashboard', [ 'table' => $data ]);
+     //    \App\Facades\HtmlTags::setTitle('OFT Dashboard');
+    	// $data = \App\Order::orderDashboard();
+    	// return view('dashboard', [ 'table' => $data ]);
+        $configSolr = Config::get('solr.demos'); 
+        $client = new Client($configSolr);
+        $query = $client->createSelect();
+        $query->setStart(0);
+        $query->setRows(1);
+        $query->addSort('id', 'desc');
+        $query->setFields('id,firstname');
+        $query->setQuery('*a*', 'firstname');
+        $hl = $query->getHighlighting();
+        $hl->setFields(array('firstname'));
+        // $dismax = $query->getDisMax();
+        // $dismax->setQueryFields('firstname');
+        $resultset = $client->select($query);
+        // var_dump( $resultset->getNumFound() );
+        // var_dump( $resultset );
+        // try {
+        //     $query->setQuery('firstname:*a*');
+            // $query->addSortField('id', \SolrQuery::ORDER_DESC);
+            // var_dump($resultset->getNumFound());
+            // var_dump($resultset);
+            // $dismax = $query->getDisMax();
+            // var_dump($dismax);
+            foreach ($resultset as $highlight) {
+                var_dump($highlight);
+            }
+        // } catch (Solarium_Exception $e) {
+        //     echo 'Ping query failed';
+        // }
     }
     public function dashboardAction()
     {
